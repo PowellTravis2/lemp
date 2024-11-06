04f4203330658d1a09b56ed1a98eead7d93ba61d
Travis Powell <developer.travis@outlook.com>
2024-11-05 21:53:02 -0700

    Working on getting the system connecting and configuring mysql

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,15 @@
 // Will run any tasks that require to be run / tested before the system boots
+// import './envConfig.ts'
+// export default defineConfig({
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//   },
+// })
+const { Sequelize } = require('sequelize');
 export async function register() {
-    console.log("Waiting...")
+    const sequelize = new Sequelize(process.env.DB_Database, process.env.DB_User, process.env.DB_Password, {
+        host: process.env.DB_Host,
+        dialect: 'mysql'
+    })
+    // console.log(process.env.DB_Host)
 }
