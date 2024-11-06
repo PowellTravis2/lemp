cb3ceb9abd737d70aa4d08214363839b4c837a30
Travis Powell <developer.travis@outlook.com>
2024-11-05 21:40:11 -0700

    Got the entire Nav Bar setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useState } from "react";
+import navStyles from '../styles/nav.module.css';
+import Link from 'next/link'
+
+interface NavbarProps {
+    navTarget: string;
+  }
+export default function Navbar({ navTarget }) {
+    return (
+        <div className={navStyles.barGroup}>
+            <ul> 
+                {/* <li>
+                    <img src="../images/lemp.png" alt="" />
+                </li> */}
+                <div className={navStyles.navGroup}>
+                    {navTarget === "home" ? (
+                        <li className={navStyles.navItem, navStyles.navItemActive}><Link href="/"><h3>Home</h3></Link></li>
+                    ) : (
+                        <li className={navStyles.navItem}><Link href="/"><h3>Home</h3></Link></li>
+                    )}
+                    {navTarget === "systems" ? (
+                        <li className={navStyles.navItem, navStyles.navItemActive}><Link href="/systems"><h3>Systems</h3></Link></li>
+                    ) : (
+                        <li className={navStyles.navItem}><Link href="/systems"><h3>Systems</h3></Link></li>
+                    )}
+                    {navTarget === "grouppolicy" ? (
+                        <li className={navStyles.navItem, navStyles.navItemActive}><Link href="/grouppolicy"><h3>Group Policies</h3></Link></li>
+                    ) : (
+                        <li className={navStyles.navItem}><Link href="/grouppolicy"><h3>Group Policies</h3></Link></li>
+                    )}
+                    {navTarget === "settings" ? (
+                        <li className={navStyles.navItem, navStyles.navItemActive}><Link href="/settings"><h3>Settings</h3></Link></li>
+                    ) : (
+                        <li className={navStyles.navItem}><Link href="/settings"><h3>Settings</h3></Link></li>
+                    )}
+                </div>
+            </ul>
+        </div>
+    );
+}
---
+++ instrumentation.ts
@@ -0,0 +1,4 @@
+// Will run any tasks that require to be run / tested before the system boots
+export async function register() {
+    console.log("Waiting...")
+}
