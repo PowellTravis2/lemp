cde69945f8423ea40701325c9632c36fe98ae200
Travis Powell <developer.travis@outlook.com>
2024-11-25 22:46:30 -0800

    Home now has content now. Bare minumum, but I couldn't figure out what to put on it. Modified NavBar to have users first name on the logout. Instead of the power button, may change it back depending.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 import Navbar from "../components/Navbar";
-import Home from "@/components/Home"; // Updated Home component
+import Home from "@/components/Home";
+import global from "@/styles/global.module.css";
 import { SessionProvider } from "next-auth/react";
 
 export default function HomePage() {
   return (
     <SessionProvider>
-      {/* The Navbar is only shown when the user is signed in */}
       <Navbar navTarget="home" />
-      <Home /> {/* Home component where useSession is used */}
+      <div className={global.usefulArea}>
+        <Home /> 
+      </div>
     </SessionProvider>
   );
 }
